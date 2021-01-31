429ec9de7caa4d6c35d5d39ac0c8b6e996e5f8a1
Valentino <valentino.r@protonmail.com>
2021-01-31 11:40:39 +0100

    added models for charts and server interaction

---
+++ src/app/shared/models/snapshot-element.interface.ts
@@ -0,0 +1,19 @@
+export interface SnapshotElement {
+  sid: number;
+  name: string;
+  topeleven: number;
+  whiteskill: number;
+  position: number;
+}
+
+export interface SnapshotAPIResponseElement {
+  id: number;
+  playerId: number;
+  timestamp: Date;
+  user: string;
+  sid: number;
+  name: string;
+  topeleven: number;
+  whiteskill: number;
+  position: number;
+}
