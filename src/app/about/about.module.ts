cf0ebeabfd84f963af7db22ea5369ab553fd7d14
Valentino <valentino.r@protonmail.com>
2021-02-07 11:03:41 +0100

    part 1 of manual

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { AboutFunctionsViewComponent } from './components/about-functions-view/about-functions-view.component';
 import { AboutInfoViewComponent } from './components/about-info-view/about-info-view.component';
-import { AboutPageComponent } from './page/about-page.component';
 import { AboutIssuesViewComponent } from './components/about-issues-view/about-issues-view.component';
+import { AboutManualViewComponent } from './components/about-manual-view/about-manual-view.component';
+import { AboutPageComponent } from './page/about-page.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     AboutInfoViewComponent,
-    AboutFunctionsViewComponent,
+    AboutManualViewComponent,
     AboutIssuesViewComponent
   ],
   imports: [
--- src/app/about/components/about-functions-view/about-functions-view.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-functions-view',
-  templateUrl: './about-functions-view.component.html',
-  styleUrls: ['./about-functions-view.component.scss']
-})
-export class AboutFunctionsViewComponent {}
